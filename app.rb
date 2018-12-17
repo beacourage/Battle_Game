6b8bfada4d1461862df642d985ab5f6b12e6adf2
beatricecourage <bea_courage@hotmail.co.uk>
2018-12-17 13:43:25 +0000

    Entering players into the game

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  # get '/' do
+  #   'Testing infrastructure working!'
+  # end
+
   get '/' do
-    'Testing infrastructure working!'
+    erb :index
+  end
+
+  post '/names' do
+    @player1_name = params[:player1_name]
+    @player2_name = params[:player2_name]
+    erb :play
   end
 
   run! if app_file == $0
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,10 @@
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
-    visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+
+feature 'Entering names' do
+  scenario 'Fill names in form and submit the form' do
+    visit ('/')
+    fill_in :player1_name, with: 'Frank'
+    fill_in :player2_name, with: 'Bea'
+    click_button 'Submit'
+    expect(page).to have_content 'Frank vs. Bea'
   end
 end
