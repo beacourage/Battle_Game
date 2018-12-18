e535ec5f5f40d00cf439ed10added0df1df4217c
beatricecourage <bea_courage@hotmail.co.uk>
2018-12-18 15:12:03 +0000

    Using a POST-redirect loop, avoid rendering view directly from POST request/route

--- app.rb
+++ app.rb
@@ -1,19 +1,42 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
   # get '/' do
   #   'Testing infrastructure working!'
   # end
 
+# Create a get '/' route that renders aindex.erb view with a form
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player1_name = params[:player1_name]
-    @player2_name = params[:player2_name]
+    session[:player1_name] = params[:player1_name]
+    session[:player2_name] = params[:player2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1_name = session[:player1_name]
+    @player2_name = session[:player2_name]
     erb :play
   end
 
-  run! if app_file == $0
+run! if app_file == $0
 end
+
+
+# First, we are extracting parameters to state. Then, we are rendering a view.
+  # Create a post '/names' route that uses params to render a play.erb view that displays the names
+
+# POST-redirect loop to avoid rendering a view directly from a POST request/route.
+# You will also use the session to store the user-submitted names between requests
+#
+# # In the post '/names' route, let's store the player names in the session instead of
+# assigning them to instance variables:
+
+# 1. submit the form to post '/names'
+# 2. extract the submitted names from the params into the session
+# 3. redirect to get '/play'
+# 4. extract the names from the session to instance variables
